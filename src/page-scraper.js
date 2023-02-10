03cfdf9729c672db22b1cfc7a9595c17be91d1b5
Aleksei Arro <polsck@gmail.com>
2023-02-10 23:43:11 +0400

    feat: receive book urls

--- src/page-scraper.js
+++ src/page-scraper.js
@@ -2,7 +2,18 @@ export const scraper = {
   url: 'http://books.toscrape.com/',
   async scrape(browser) {
     const page = await browser.newPage();
-    console.log(`Navigating to ${this.url}...`)
+
+    console.log(`Navigating to ${this.url}...`);
     await page.goto(this.url);
+
+    console.log('Receiving urls of books in stock');
+    await page.waitForSelector('.page_inner');
+    const bookUrls = await page.$$eval('section ol > li', (links) => {
+      const booksInStock = links.filter((link) => link.querySelector('.instock.availability').textContent.trim() === 'In stock');
+
+      return booksInStock.map((link) => link.querySelector('h3 > a').href);
+    });
+
+    console.log('Book urls:', bookUrls);
   },
 };
